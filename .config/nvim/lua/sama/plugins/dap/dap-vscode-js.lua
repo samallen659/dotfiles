9b453bb116ca54330eb71f5a6d898b4cba1026db
Sam Allen <samallen659@gmail.com>
2023-04-09 17:19:32 +0100

    trying to get dap working

---
+++ .config/nvim/lua/sama/plugins/dap/dap-vscode-js.lua
@@ -0,0 +1,61 @@
+require("dap-vscode-js").setup({
+	node_path = "node",
+	debugger_path = "~/.local/share/nvim/site/pack/packer/opt/vscode-js-debug",
+	debugger_cmd = { "js-debug-adapter" },
+	adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+})
+
+for _, language in ipairs({ "typescript", "javascript" }) do
+	require("dap").configurations[language] = {
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch file",
+			program = "${file}",
+			cwd = "${workspaceFolder}",
+		},
+		{
+			type = "pwa-node",
+			request = "attach",
+			name = "Attach",
+			proccessId = require("dap.utils").pick_process,
+			cwd = "${workspaceFolder}",
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Debug Jest Tests",
+			runtimeExecutable = "node",
+			runtimeArgs = {
+				"./node_modules/jest/bin/jest.js",
+				"--runInBand",
+			},
+			rootPath = "${workspaceFolder}",
+			cwd = "${workspaceFolder}",
+			console = "integratedTerminal",
+			internalConsoleOptions = "neverOpen",
+		},
+	}
+end
+
+for _, language in ipairs({ "typescriptreact", "javascriptreact" }) do
+	require("dap").configurations[language] = {
+		{
+			type = "pwa-chrome",
+			name = "Attach - Remote Debugging",
+			request = "attach",
+			program = "${file}",
+			cwd = vim.fn.getcwd(),
+			sourceMaps = true,
+			protocol = "inspector",
+			port = 9222,
+			webRoot = "${workspaceFolder}",
+		},
+		{
+			type = "pwa-chrome",
+			name = "Launch Chrome",
+			request = "launch",
+			url = "http://localhost:3000",
+		},
+	}
+end
