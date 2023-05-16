1e92ebca9b15230e8e540c5853f7ded02e28e7c5
Sam Allen <samallen659@gmail.com>
2023-05-16 18:59:29 +0100

    added tabout config and base js & ts dap setup

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -6,45 +6,151 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.relativenumber = true
 
-lvim.colorscheme="moonfly"
+lvim.colorscheme = "moonfly"
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel=99
-vim.opt.foldlevelstart=99
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
 
 local autoCommands = {
     -- other autocommands
     open_folds = {
-        {"BufReadPost,FileReadPost", "*", "normal zR"}
+        { "BufReadPost,FileReadPost", "*", "normal zR" }
     }
 }
 
 
 -- Additional Plugins
 lvim.plugins = {
-    {"lunarvim/colorschemes"},
-    {"abecodes/tabout.nvim"},
-	{"bluz71/vim-moonfly-colors"},
-    {"theprimeagen/harpoon"}
+    { "lunarvim/colorschemes" },
+    { "abecodes/tabout.nvim" },
+    { "bluz71/vim-moonfly-colors" },
+    { "theprimeagen/harpoon" },
+    { "mxsdev/nvim-dap-vscode-js" },
+    {
+        "microsoft/vscode-js-debug",
+        lazy = true,
+        build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+    },
+    { "christoomey/vim-tmux-navigator" },
 }
 
+-- Tabout
+require('tabout').setup {
+    tabkey = '<Tab>',             -- key to trigger tabout, set to an empty string to disable
+    backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+    act_as_tab = true,            -- shift content if tab out is not possible
+    act_as_shift_tab = false,     -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+    default_tab = '<C-t>',        -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+    default_shift_tab = '<C-d>',  -- reverse shift default action,
+    enable_backwards = true,      -- well ...
+    completion = true,            -- if the tabkey is used in a completion pum
+    tabouts = {
+        { open = "'", close = "'" },
+        { open = '"', close = '"' },
+        { open = '`', close = '`' },
+        { open = '(', close = ')' },
+        { open = '[', close = ']' },
+        { open = '{', close = '}' }
+    },
+    ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+    exclude = {} -- tabout will ignore these filetypes
+}
 
+-- window spliting
 lvim.builtin.which_key.mappings["w"] = {
     name = "Window",
-    v = {"<cmd>vsplit<cr>","Vertical Split"},
-    h = {"<cmd>split<cr>", "Horizontal Split"}
+    v = { "<cmd>vsplit<cr>", "Vertical Split" },
+    h = { "<cmd>split<cr>", "Horizontal Split" }
 }
 
-
-local mark = require("harpoon.mark")
+-- Harpoon
 local ui = require("harpoon.ui")
 
 lvim.builtin.which_key.mappings["a"] = {
-
-		name = "Harpoon",
-		a = { '<cmd>lua require("harpoon.mark").add_file()<CR>', "Add File" },
-		r = { '<cmd>lua require("harpoon.mark").rm_file()<CR>', "Remove File" },
-		c = { '<cmd>lua require("harpoon.mark").clear_all()<CR>', "Clear List" },
+    name = "Harpoon",
+    a = { '<cmd>lua require("harpoon.mark").add_file()<CR>', "Add File" },
+    r = { '<cmd>lua require("harpoon.mark").rm_file()<CR>', "Remove File" },
+    c = { '<cmd>lua require("harpoon.mark").clear_all()<CR>', "Clear List" },
 }
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+-- FORMATTER
+lvim.format_on_save = true -- Change this to false if you prefer
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    {
+        command = "prettier",
+        extra_args = { "--print-width", "120" },
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    },
+}
+
+lvim.builtin.dap.active = true
+
+local debugger_path = os.getenv("LUNARVIM_RUNTIME_DIR") .. "/site/pack/lazy/opt/vscode-js-debug"
+
+require("dap-vscode-js").setup {
+    node_path = "node",
+    debugger_path = debugger_path,
+    -- debugger_cmd = { "js-debug-adapter" },
+    adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+}
+
+for _, language in ipairs { "typescript", "javascript" } do
+    require("dap").configurations[language] = {
+        {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+        },
+        {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+        },
+        {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Jest Tests",
+            -- trace = true, -- include debugger info
+            runtimeExecutable = "node",
+            runtimeArgs = {
+                "./node_modules/jest/bin/jest.js",
+                "--runInBand",
+            },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+        },
+    }
+end
+
+for _, language in ipairs { "typescriptreact", "javascriptreact" } do
+    require("dap").configurations[language] = {
+        {
+            type = "pwa-chrome",
+            name = "Attach - Remote Debugging",
+            request = "attach",
+            program = "${file}",
+            cwd = vim.fn.getcwd(),
+            sourceMaps = true,
+            protocol = "inspector",
+            port = 9222,
+            webRoot = "${workspaceFolder}",
+        },
+        {
+            type = "pwa-chrome",
+            name = "Launch Chrome",
+            request = "launch",
+            url = "http://localhost:3000",
+        },
+    }
+end
