798795946e4f6bf8a2453e3f33669139763407dd
Sam Allen <samallen659@gmail.com>
2024-08-09 20:16:37 +0100

    fixes nvim markdown code highlighting

--- .config/nvim/lua/sama/lazy/treesitter.lua
+++ .config/nvim/lua/sama/lazy/treesitter.lua
@@ -1,3 +1,23 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "dockerfile",
+                "bicep",
+                "go",
+                "helm",
+                "json",
+                "python",
+                "sql",
+                "toml",
+                "yaml",
+            },
+            highlight = {
+                enable = true
+            }
+        })
+    end,
 }
