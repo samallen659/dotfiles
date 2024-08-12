17ef4bb6114ddd1273627e9c6890512323690094
Sam Allen <samallen659@gmail.com>
2024-08-12 20:06:34 +0100

    adds additional nvim remaps

--- .config/nvim/lua/sama/remap.lua
+++ .config/nvim/lua/sama/remap.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = " "
 
--- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 vim.keymap.set("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit<cr>")
+vim.keymap.set("n", "<leader>w", "<cmd>split<cr>")
+
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
