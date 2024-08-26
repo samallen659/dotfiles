53fb550dc7602e40c34b8af712b804b92db53659
Sam Allen <samallen659@gmail.com>
2024-08-26 20:27:03 +0100

    adds shortcut to remove highlight

--- .config/nvim/lua/sama/remap.lua
+++ .config/nvim/lua/sama/remap.lua
@@ -10,3 +10,5 @@ vim.keymap.set("n", "<leader>w", "<cmd>split<cr>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>h", "<cmd>noh<cr>")
