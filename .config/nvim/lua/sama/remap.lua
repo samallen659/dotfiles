7ce29f840eeb94c8c4929fcab7ade6d5b43f3d19
Sam Allen <samallen659@gmail.com>
2024-12-26 15:02:18 +0000

    adds nvim remap for set wrap

--- .config/nvim/lua/sama/remap.lua
+++ .config/nvim/lua/sama/remap.lua
@@ -10,3 +10,5 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>h", "<cmd>noh<cr>")
+
+vim.keymap.set("n", "<leader>sw", "<cmd>set wrap<cr>")
