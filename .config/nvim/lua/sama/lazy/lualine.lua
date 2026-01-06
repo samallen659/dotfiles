9b44151af1763067d724beefcea8f57ceb356d90
samallennlc <sam.allen@northlincs.gov.uk>
2026-01-06 13:16:16 +0000

    merge neovim terraformls new file fix

--- .config/nvim/lua/sama/lazy/lualine.lua
+++ .config/nvim/lua/sama/lazy/lualine.lua
@@ -1,11 +1,21 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local lualine = require("lualine")
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
 
-        lualine.setup({
-            options = { theme =  "moonfly" }
-        })
-    end,
+		lualine.setup({
+			options = {
+				theme = "moonfly",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 3,
+					},
+				},
+			},
+		})
+	end,
 }
