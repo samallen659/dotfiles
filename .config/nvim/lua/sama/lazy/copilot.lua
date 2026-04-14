9628f52bb90930c5db487b55bebfd4118c644182
samallennlc <sam.allen@northlincs.gov.uk>
2026-04-14 16:35:56 +0100

    adds copilot commands

--- .config/nvim/lua/sama/lazy/copilot.lua
+++ .config/nvim/lua/sama/lazy/copilot.lua
@@ -1,3 +1,7 @@
 return {
 	"github/copilot.vim",
+	keys = {
+		{ "<leader>ce", "<cmd>Copilot enable<CR>", desc = "Enable Copilot" },
+		{ "<leader>co", "<cmd>Copilot disable<CR>", desc = "Disable Copilot" },
+	},
 }
